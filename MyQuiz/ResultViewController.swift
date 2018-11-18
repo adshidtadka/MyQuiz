0bd893cccaa6216ef96054da24c352cbce819156
gky360 <gky360@gmail.com>
2018-11-18 13:20:53 +0900

    add ResultViewController

--- MyQuiz/ResultViewController.swift
+++ MyQuiz/ResultViewController.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    @IBOutlet weak var correctPercentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let questionCount = QuestionDataManager.sharedInstance.questionDataArray.count
+        var correctCount :Int = 0
+        for questionData in QuestionDataManager.sharedInstance.questionDataArray {
+            if questionData.isCollect() {
+                correctCount += 1
+            }
+        }
+        
+        let correctPercent: Float = (Float(correctCount) / Float(questionCount)) * 100
+        
+        correctPercentLabel.text = String(format: "%.1f", correctPercent) + "%"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
